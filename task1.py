d9eb6eef8af5900f1c6652e9e3511dee5efe2aef
Yulia Chornenko <44753645+YuliaChornenko@users.noreply.github.com>
2018-12-04 10:49:42 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -5,22 +5,29 @@ from data import dataset
 
 
 def addStudentSkill(student, company, skill):
-    #TODO
+    if student in dataset:
+
+        if company in dataset[student]:
+            print_list=dataset[student][company]
+            print_list.add(skill)
+        else:
+            dataset[student][company]={skill}
+    else:
+        dataset[student]={company: {skill}}
 
 
 
 print("Task 1")
 
 #Додати нового студента та вміння у новій компанії
-addStudentSkill(?,?,?)
+addStudentSkill("KB-13131313","bankaval","bepatient")
 
 #Додати існуючому студенту нове вміння у новій компанії
-addStudentSkill(?,?,?)
+addStudentSkill("KB-12121212","atb","smileeverytime")
 
 #Додати існуючому студенту нове вміння в існуючій компанії
-addStudentSkill(?,?,?)
+addStudentSkill("KB-12121212","privat24","be_patient")
 
 print(dataset)
-
 
 print("\n\n")
